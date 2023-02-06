c55325006b09f4da90444a6aa1f775db53245367
Sunil Chaudhary <sunilchaudhary2304@gmail.com>
2023-02-05 18:09:05 -0600

    promote modified code

--- Situationships.Client/src/app/members/member-messages/member-messages.component.ts
+++ Situationships.Client/src/app/members/member-messages/member-messages.component.ts
@@ -12,6 +12,7 @@ export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string;
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -20,8 +21,9 @@ export class MemberMessagesComponent implements OnInit {
 
   sendMessage() {
     if (!this.username) return;
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm?.reset();
-    })
+    }).finally(() => this.loading = false)
   }
 }
